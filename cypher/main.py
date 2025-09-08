780db37dbb547af4e626658e6451402cfae8e6a3
SystemCRM24 <admin@systemcrm.ru>
2025-09-08 11:23:43 +0300

    init cypher app

--- cypher/main.py
+++ cypher/main.py
@@ -1,1 +1,19 @@
-'Пасхалка'
+from fastapi import FastAPI
+
+from src.common import router as common_router
+
+
+description = """Предоставляет методы для отправки и получения запросов в ГИИС ДМДК.
+Содержит полную логику этого процесса:
+1) Добавление подписи в SOAP-сообщение.
+2) Отправка SOAP-сообщения в ГИИС-ДМДК
+3) Проверка валидности сообщения от ГИИС-ДМДК
+4) Ряд утилитарных методов для работы с подписями, шифрованием и тп.
+"""
+
+app = FastAPI(
+    title='Cypher',
+    summary='Интерфейс для отправки SOAP-запросов в ГИИС.',
+    description=description
+)
+app.include_router(common_router)
