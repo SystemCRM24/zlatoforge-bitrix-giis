890f8ac22d7f4eede67360d27290bad949b4faa4
dfx_17 <rossohinpavel@yandex.ru>
2025-11-14 17:05:14 +0300

    fix

--- app/src/api/clients.py
+++ app/src/api/clients.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="")
 
 
 @router.post("/check_contact")
-async def check_contact(clinet_id: str = Query(), user_id: str = Query()):
+async def check_contact(client_id: str = Query(), user_id: str = Query()):
     """Метод для получения сведений о причастности лиа к экстремизму или терорризму."""
-    coro = check_bitrix_contact(clinet_id, user_id)
+    coro = check_bitrix_contact(client_id, user_id)
     asyncio.create_task(coro)
